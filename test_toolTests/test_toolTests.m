4eeaf4f62bb35d9af3df9927dcd5fb24c80ade74
mac <mac@macdeimac.local>
2018-11-29 15:18:36 +0800

    Initial Commit

---
+++ test_toolTests/test_toolTests.m
@@ -0,0 +1,37 @@
+//
+//  test_toolTests.m
+//  test_toolTests
+//
+//  Created by mac  on 2018/11/29.
+//  Copyright © 2018年 mac . All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface test_toolTests : XCTestCase
+
+@end
+
+@implementation test_toolTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
